6b3bfba07708584ebedd5d9a845289ae7b9c4221
Jack Koss <kossjack8@gmail.com>
2023-07-01 03:21:11 -0500

    added svelte instead of figuring out jsx

---
+++ src/components/SocialLinks.svelte
@@ -0,0 +1,8 @@
+<script>
+    let platforms = ['github', 'fortnite', 'instagram', 'linkedin', 'twitter', 'twitch', 'youtube'];
+    let username = 'kossj';
+</script>
+
+{#each platforms as platform, i}
+    <a href="https://{platform}.com/{username}">{platform}</a>{#if i < platforms.length - 1}&nbsp;{/if}
+{/each}
